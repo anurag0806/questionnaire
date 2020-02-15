ea1eeb6797ca82816641be86a53b08c8f52fa6c6
Anurag <developer0806@gmail.com>
2020-02-16 00:00:43 +0530

    initial commit

---
+++ questionnaire project.mm
@@ -0,0 +1,92 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="questionnaire project" FOLDED="false" ID="ID_1342858745" CREATED="1581754100446" MODIFIED="1581754398427" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" background="#3c3f41">
+    <properties edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#cccccc" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#cccccc" BACKGROUND_COLOR="#3c3f41" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#dddddd" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ff3300">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffb439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#99ffff">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#bbbbbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="13" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="html for questions" POSITION="left" ID="ID_1747258081" CREATED="1581754115164" MODIFIED="1581754124899">
+<edge COLOR="#00dddd"/>
+</node>
+<node TEXT="image for branding" POSITION="left" ID="ID_101526032" CREATED="1581754190882" MODIFIED="1581754207457">
+<edge COLOR="#dddd7c"/>
+<node TEXT="SVG?" ID="ID_562361938" CREATED="1581754210797" MODIFIED="1581754213385"/>
+</node>
+<node TEXT="Drag drop branding to select answers" POSITION="left" ID="ID_689044339" CREATED="1581754215898" MODIFIED="1581754231070">
+<edge COLOR="#7cdd7c"/>
+</node>
+<node TEXT="JS to count answers" POSITION="left" ID="ID_1926733431" CREATED="1581754125873" MODIFIED="1581754142956">
+<edge COLOR="#dddd00"/>
+</node>
+<node TEXT="keep questions external" POSITION="right" ID="ID_1507669801" CREATED="1581754600937" MODIFIED="1581754683283">
+<edge COLOR="#7c7cdd"/>
+<node TEXT="json?" ID="ID_1776927252" CREATED="1581754684836" MODIFIED="1581754687598"/>
+<node TEXT="local" ID="ID_1324142473" CREATED="1581754689606" MODIFIED="1581754691352"/>
+</node>
+<node TEXT="css for drag and drop slection" POSITION="right" ID="ID_860283331" CREATED="1581759771893" MODIFIED="1581759786508">
+<edge COLOR="#00dddd"/>
+</node>
+<node TEXT="how to calculate results" POSITION="right" ID="ID_1750113089" CREATED="1581759786976" MODIFIED="1581759797368">
+<edge COLOR="#dddd00"/>
+</node>
+</node>
+</map>
